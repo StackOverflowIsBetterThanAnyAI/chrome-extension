b6b4932c8a64eead8a7be0d5c2b5892a9ac6d9cf
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2024-04-27 14:37:34 +0200

    animated number generation

--- script.js
+++ script.js
@@ -4,38 +4,97 @@ document.addEventListener('DOMContentLoaded', function () {
 })
 
 const generateNumber = () => {
-    const minElement = parseInt(document.getElementById('minInput')?.value)
-    const maxElement = parseInt(document.getElementById('maxInput')?.value)
+    const minElement = document.getElementById('minInput')
+    const maxElement = document.getElementById('maxInput')
+    const minValue = parseInt(minElement?.value)
+    const maxValue = parseInt(maxElement?.value)
+
     const errorElement = document.getElementById('error')
     const resultElement = document.getElementById('result')
     const resultElementEquals = document.getElementById('resultEquals')
+    const generateButton = document.getElementById('generateButton')
+
+    const pointOne = document.getElementById('point1')
+    const pointTwo = document.getElementById('point2')
+    const pointThree = document.getElementById('point3')
+
+    const loaderContainer = document.getElementById('loaderContainer')
 
     if (
-        isNaN(minElement) ||
-        minElement > 999999999999999 ||
-        minElement < -999999999999999
+        isNaN(minValue) ||
+        minValue > 999999999999999 ||
+        minValue < -999999999999999
     ) {
-        errorElement.style.visibility = 'visible'
-        resultElement.style.visibility = 'hidden'
+        errorElement.hidden = false
+        resultElement.hidden = true
+        loaderContainer.hidden = true
         return
     }
     if (
-        isNaN(maxElement) ||
-        maxElement > 999999999999999 ||
-        maxElement < -999999999999999
+        isNaN(maxValue) ||
+        maxValue > 999999999999999 ||
+        maxValue < -999999999999999
     ) {
-        errorElement.style.visibility = 'visible'
-        resultElement.style.visibility = 'hidden'
+        errorElement.hidden = false
+        resultElement.hidden = true
+        loaderContainer.hidden = true
         return
     }
 
-    const min = minElement < maxElement ? minElement : maxElement
-    const max = min === minElement ? maxElement : minElement
+    const min = minValue < maxValue ? minValue : maxValue
+    const max = min === minValue ? maxValue : minValue
 
     if (!isNaN(min) && !isNaN(max)) {
+        loaderContainer.hidden = false
+        setTimeout(() => handleCalculation(), 1125)
+        handleHidden()
+        handleDisabled()
+        handleAnimation()
+        setTimeout(() => handleEnabled(), 1125)
+    }
+
+    function handleCalculation() {
         const result = Math.floor(Math.random() * (max - min + 1)) + min
-        errorElement.style.visibility = 'hidden'
-        resultElement.style.visibility = 'visible'
         resultElementEquals.innerHTML = result
+    }
+
+    function handleHidden() {
+        errorElement.hidden = true
+        resultElement.hidden = false
+    }
+
+    function handleDisabled() {
+        generateButton.setAttribute('disabled', 'true')
+        maxElement.setAttribute('disabled', 'true')
+        minElement.setAttribute('disabled', 'true')
+        resultElement.style.cursor = 'not-allowed'
+        resultElement.style.backgroundColor = '#c8c8c8'
+        resultElement.style.opacity = '0.75'
+        resultElement.style.border = '1px solid #4b4b4b'
+    }
+
+    function handleEnabled() {
+        generateButton.removeAttribute('disabled')
+        maxElement.removeAttribute('disabled')
+        minElement.removeAttribute('disabled')
+        resultElement.style.cursor = 'default'
+        resultElement.style.backgroundColor = '#f5f5f5'
+        resultElement.style.opacity = '1'
+        resultElement.style.border = 'none'
+    }
+
+    function handleAnimation() {
+        pointOne.style.visibility = 'visible'
+        setTimeout(() => (pointTwo.style.visibility = 'visible'), 125)
+        setTimeout(() => (pointThree.style.visibility = 'visible'), 250)
+        setTimeout(() => (pointOne.style.visibility = 'hidden'), 375)
+        setTimeout(() => (pointTwo.style.visibility = 'hidden'), 500)
+        setTimeout(() => (pointThree.style.visibility = 'hidden'), 625)
+        setTimeout(() => (pointOne.style.visibility = 'visible'), 750)
+        setTimeout(() => (pointTwo.style.visibility = 'visible'), 875)
+        setTimeout(() => (pointThree.style.visibility = 'visible'), 1000)
+        setTimeout(() => (pointOne.style.visibility = 'hidden'), 1125)
+        setTimeout(() => (pointTwo.style.visibility = 'hidden'), 1125)
+        setTimeout(() => (pointThree.style.visibility = 'hidden'), 1125)
     }
 }
